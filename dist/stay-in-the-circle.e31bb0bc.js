9aec8e40057f703f5e54d3904e7767bdf04b2ffd
darkthedev <darkthedev.x@gmail.com>
2020-08-26 18:52:32 +0200

    first commit

--- dist/stay-in-the-circle.e31bb0bc.js
+++ dist/stay-in-the-circle.e31bb0bc.js
@@ -28479,7 +28479,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52544" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49411" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
